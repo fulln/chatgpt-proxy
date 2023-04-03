544ad1690934cd974f706efd7fc86c5e66f9b0a8
fulln <a114mypr1nt@gmail.com>
2023-04-03 19:24:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,24 +1,25 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if(url.pathname.include("fulln")){
-      url.host = "chat-gpt-fulln.vercel.app";
-      // openai is already set all CORS heasders 
-      return fetch(url, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body,
-        redirect: 'follow'
-      });
-    }else{
-      url.host = "api.openai.com";
-      // openai is already set all CORS heasders 
-      return fetch(url, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body,
-        redirect: 'follow'
-      });
+    async fetch(request, env) {
+      const url = new URL(request.url);
+      if(url.pathname.includes("fulln")){
+        url.host = "chat-gpt-fulln.vercel.app";
+        // openai is already set all CORS heasders 
+        return fetch(url, {
+          headers: request.headers,
+          method: request.method,
+          body: request.body,
+          redirect: 'follow'
+        });
+      }else{
+        url.host = "api.openai.com";
+        // openai is already set all CORS heasders 
+        return fetch(url, {
+          headers: request.headers,
+          method: request.method,
+          body: request.body,
+          redirect: 'follow'
+        });
+      }
     }
   }
-}
+  
